fc895b79b55088a338b9b47e7ae3ea706620a823
waheng <peterchang@yahoo-inc.com>
2018-03-31 21:53:52 +0800

    added : updateById(createdObjId, payload)

--- model/costs.js
+++ model/costs.js
@@ -1,49 +1,55 @@
 const CLASS_NAME = 'Cost'
 
 const create = (payload) => {
-    if(!payload.cost) return Promise.reject(new Error('[Missing field] cost is required'))
-    if(!payload.userId) return Promise.reject(new Error('[Missing field] userId is required'))
+  if (!payload.cost) return Promise.reject(new Error('[Missing field] cost is required'))
+  if (!payload.userId) return Promise.reject(new Error('[Missing field] userId is required'))
 
-    const Cost = new (AV.Object.extend(CLASS_NAME))()
-    return Cost.save(payload)
+  const Cost = new (AV.Object.extend(CLASS_NAME))()
+  return Cost.save(payload)
 }
 
-const list = (payload = {}, field={}, exist={}) => {
-    const {
-        limit = 10,
-        skip =0,
-        ascending,
-        descending = 'createdAt',
-        greaterThan,
-        lessThan
-    } = payload
+const list = (payload = {} , field = {} , exist = {}) => {
+  const { limit = 10, skip =0, ascending, descending = 'createdAt', greaterThan, lessThan} = payload
 
-    var query = new AV.Query(CLASS_NAME)
+  var query = new AV.Query(CLASS_NAME)
 
-    if(limit) query.limit(limit)
+  if (limit) query.limit(limit)
 
-    if(skip) query.skip(skip)
+  if (skip) query.skip(skip)
 
-    if(greaterThan && greaterThan.field)
-        query.greaterThan(greaterThan.field, greaterThan.val)
+  if (greaterThan && greaterThan.field)
+    query.greaterThan(greaterThan.field, greaterThan.val)
 
-    if(lessThan && lessThan.field)
-        query.lessThan(lessThan.field, lessThan.val)
+  if (lessThan && lessThan.field)
+    query.lessThan(lessThan.field, lessThan.val)
 
-    if(ascending)
-        query.ascending(ascending)
-    else 
-        query.descending(descending)
+  if (ascending)
+    query.ascending(ascending)
+  else
+    query.descending(descending)
 
-    query.doesNotExist('disable')
-    return query.find()
+  query.doesNotExist('disable')
+  return query.find()
+}
+
+const updateById = (id, payload = {}) => {
+  var query = new AV.Query(CLASS_NAME)
+
+  return query.get(id).then(function (_obj) {
+      
+    Object.entries(payload).forEach(([key, val]) => {
+      _obj.set(key, val)
+    })
+
+    return _obj.save()
+  })
 }
 
 const remove = (id) => {
-    if(!id) return Promise.reject(new Error('[Missing field] id is required'))
+  if (!id) return Promise.reject(new Error('[Missing field] id is required'))
 
-    const Cost = AV.Object.createWithoutData(CLASS_NAME, id);
-    return Cost.destroy()
+  const Cost = AV.Object.createWithoutData(CLASS_NAME, id)
+  return Cost.destroy()
 }
 
-module.exports = {create, remove, list}
+module.exports = {create, remove, list, updateById}
