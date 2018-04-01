907535b7b1835569380c2a3577470537e9ed4b26
waheng <peterchang@yahoo-inc.com>
2018-04-01 16:01:55 +0800

    Redirect to redirect_uri

--- routers/login/index.js
+++ routers/login/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
     console.log('-=-=-=-= GET webhook -=-=-=-=')
-    const content = `<pre> ${JSON.stringify(req.normal, null, 2)}</pre>`
-    return res.send(content)
+    console.log(req.normal)
+    return res.redirect(req.normal.redirect_uri);
+    // const content = `<pre> ${JSON.stringify(req.normal, null, 2)}</pre>`
+    // return res.send(content)
 })
 
 module.exports = router
