c2e72441c44bec728b86349592cfeb5bb1c2c745
waheng <peterchang@yahoo-inc.com>
2018-04-01 16:45:15 +0800

    no message

--- routers/login/index.js
+++ routers/login/index.js
@@ -4,10 +4,10 @@ const router = express.Router()
 router.get('/', (req, res) => {
     console.log('-=-=-=-= GET webhook -=-=-=-=')
     console.log(req.normal)
-    const redirUrl = `${req.normal.redirect_uri}&authorization_code=abc`
-    return res.redirect(redirUrl);
-    // const content = `<pre> ${JSON.stringify(req.normal, null, 2)}</pre>`
-    // return res.send(content)
+    // const redirUrl = `${req.normal.redirect_uri}&authorization_code=abc`
+    // return res.redirect(redirUrl);
+    const content = `<pre> ${JSON.stringify(req.normal, null, 2)}</pre>`
+    return res.send(content)
 })
 
 module.exports = router
